9fba029189235d92cb8716b917c848b506383236
dariameln1 <dariamelnykova1@gmail.com>
2024-02-03 18:31:18 +0100

    commit

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,9 +1,18 @@
 const Joi = require("joi");
 
-exports.createContactSchema = Joi.object({
-
+const createContactSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
 });
 
-exports.updateContactSchema = Joi.object({
+const updateContactSchema = Joi.object({
+  name: Joi.string(),
+  email: Joi.string(),
+  phone: Joi.string(),
+}).min(1);
 
-});
+module.exports = {
+  createContactSchema,
+  updateContactSchema,
+};
