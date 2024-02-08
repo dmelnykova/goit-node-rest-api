b02c0e843a217c15d9a31214134b4461c51ff48b
dariameln1 <dariamelnykova1@gmail.com>
2024-02-08 15:04:15 +0100

    Update getInfo.js

--- controllers/usersController/getInfo.js
+++ controllers/usersController/getInfo.js
@@ -3,21 +3,22 @@ const User = require("../../models/usersModels/users");
 
 const getInfo = async (req, res, next) => {
   const authHeader = req.headers.authorization || "";
+
   const [type, token] = authHeader.split(" ");
 
   if (!token) {
     throw HttpError(401, "Not authorized");
   }
 
-  const user = await User.findOne({ token });
+  const user = await User.findOne({ token: token });
+
+  const { email, subscription } = user;
 
   if (!user) {
     throw HttpError(401, "Not authorized");
   }
 
-  const { email, subscription } = user;
-
-  res.json({ email, subscription });
+  res.json({email, subscription});
 };
 
 module.exports = getInfo;
